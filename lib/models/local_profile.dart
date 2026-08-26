8b4874b51136e377a9e54276b46a685928878987
dwilliams-sketch <dwilliams@dwsafety.co.uk>
2026-08-26 14:26:44 +0100

    Add files via upload

---
+++ lib/models/local_profile.dart
@@ -0,0 +1,6 @@
+class LocalProfile {
+  final String displayName;
+  final String email;
+  final String clubName;
+  const LocalProfile({required this.displayName, required this.email, required this.clubName});
+}
